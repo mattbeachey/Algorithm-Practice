7a201e0afbef6a039b0dc38c1c09c5ef9caf60f0
Matt Beachey <mattbeachey@gmail.com>
2020-05-17 22:40:41 -0500

    Refactored to allow for indefinite parameters, added tests, dried up code

--- JS/symmetric-difference.js
+++ JS/symmetric-difference.js
@@ -1,27 +1,28 @@
+
+
+
+
 //Symmetric Difference
 // https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/find-the-symmetric-difference
-//TODO: has to be able to take more than two arguments: shit.
+/*
+Create a function that takes two or more arrays and returns an array of the symmetric 
+difference (△ or ⊕) of the provided arrays.
 
-//add all params into an array
-//loop thru array - group pairs together to run thru test: 0 and 1, 2 and 3, etc.
-//if one of a pair equals null, bypass the test and just return the other array
-//if both equal null, just return;
-//will need recursion
+Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term 
+"symmetric difference" of two sets is the set of elements which are in either of the two sets, 
+but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say 
+on a set D = {2, 3}), you should get the set with elements which are in either of the two the 
+sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}). The resulting array must contain 
+only unique values (no duplicates).
+*/
 
-
-function sym(a, b, c, d, e, f, g, h) {
-    a = a || null;
-    b = b || null;
-    c = c || null;
-    d = d || null;
-    e = e || null;
-    f = f || null;
-    g = g || null;
-    h = h || null;
-  
+function sym(...args) {
     const bigArray = []
-    bigArray.push(a, b, c, d, e, f, g, h)
-  
+    for (const key of args){
+      if (typeof(key) === "object"){
+        bigArray.push(key)
+      }
+    }
     return recur(bigArray)
   }
   
@@ -33,14 +34,10 @@ function sym(a, b, c, d, e, f, g, h) {
       }
     }
   
-    const noUnd = nextArray.filter(function (item) {
-      return item !== undefined;
-    });
-  
-    if (noUnd.length > 1){
-      return recur(noUnd)
-    } else if (noUnd.length === 1){
-      return noUnd[0];
+    if (nextArray.length > 1){
+      return recur(nextArray)
+    } else if (nextArray.length === 1){
+      return nextArray[0];
     }
   
   }
@@ -54,7 +51,7 @@ function sym(a, b, c, d, e, f, g, h) {
     } else if (array2 !== null && array1 !== null || array2 !== undefined && array1 !== undefined){
       const first = testSym(array1, array2)
       const second = testSym(array2, array1)
-      const sorted = first.concat(second).sort(function (a, b) {
+      const sorted = first.concat(second).sort((a, b)=>{
         return a - b;
       });
       const uniqArray = []
@@ -76,9 +73,39 @@ function sym(a, b, c, d, e, f, g, h) {
         }
       }
     }
-    return cloneArray.filter(function (item) {
+    return cloneArray.filter(item => {
       return item !== null;
     });
   }
-  
-  console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]))
+
+//Tests
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6, "the"], [1, 2, 3], [5, 3, 9, 8], [1], ["the", "quick", "brown", "fox"]))
+
+console.log(sym([1, 2, 3], [5, 2, 1, 4]))
+console.log("Test 1 passed: ", arraysEqual(sym([1, 2, 3], [5, 2, 1, 4]), [ 3, 4, 5 ]));
+console.log("Test 2 passed: ", arraysEqual(sym([1, 2, 3, 3], [5, 2, 1, 4]), [3, 4, 5]));
+console.log("Test 3 passed: ", arraysEqual(sym([1, 2, 3], [5, 2, 1, 4, 5]), [3, 4, 5]));
+console.log("Test 4 passed: ", arraysEqual(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]), [1, 4, 5]));
+console.log("Test 5 passed: ", arraysEqual(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]), [1, 4, 5]));
+console.log("Test 6 passed: ", arraysEqual(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]), [2, 3, 4, 6, 7]));
+console.log("Test 7 passed: ", arraysEqual(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]), [1, 2, 4, 5, 6, 7, 8, 9]));
+
+
+
+
+//compare arrays method for testing: https://stackoverflow.com/questions/6229197/how-to-know-if-two-arrays-have-the-same-values
+function arraysEqual(_arr1, _arr2) {
+    if (!Array.isArray(_arr1) || ! Array.isArray(_arr2) || _arr1.length !== _arr2.length)
+      return false;
+
+    var arr1 = _arr1.concat().sort();
+    var arr2 = _arr2.concat().sort();
+
+    for (var i = 0; i < arr1.length; i++) {
+
+        if (arr1[i] !== arr2[i])
+            return false;
+    }
+    return true;
+
+}
