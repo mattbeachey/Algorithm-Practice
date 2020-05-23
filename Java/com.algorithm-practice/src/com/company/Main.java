c856dc4d6549f4fe510e8689ca27e207e1a71c52
Matt Beachey <mattbeachey@gmail.com>
2020-05-23 16:03:45 -0500

    set up java structure

---
+++ Java/com.algorithm-practice/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	test();
+    }
+
+    private static void test(){
+        System.out.println("sup");
+    }
+
+}
