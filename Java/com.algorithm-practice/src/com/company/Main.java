3592c1e48c93af2f34f23ef2261734fd468a261f
testuser <testuser@example.com>
2020-07-24 16:58:56 -0500

    added loop order/magnitude visuilization

--- Java/com.algorithm-practice/src/com/company/Main.java
+++ Java/com.algorithm-practice/src/com/company/Main.java
@@ -3,25 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Symmetric Difference
-        SymmDiff.runTest();
-
-        //Digital Product
-        System.out.println(DigitalProduct.digitsProduct(4));
-
-        //Percent of What
-        int[] answer = PercentOfWhat.percentOfWhat(53.5);
-        System.out.println(answer[0]);
-        System.out.println("out of");
-        System.out.println(answer[1]);
-
-        //Rectangle Rotation (https://app.codesignal.com/arcade/code-arcade/labyrinth-of-nested-loops/tuKA5f3zpzkKKejJx)
-        System.out.println(RectangleRotation.recRot(6, 4));
+//        //Symmetric Difference
+//        SymmDiff.runTest();
+//
+//        //Digital Product
+//        System.out.println(DigitalProduct.digitsProduct(4));
+//
+//        //Percent of What
+//        int[] answer = PercentOfWhat.percentOfWhat(53.5);
+//        System.out.println(answer[0]);
+//        System.out.println("out of");
+//        System.out.println(answer[1]);
+//
+//        //Rectangle Rotation (https://app.codesignal.com/arcade/code-arcade/labyrinth-of-nested-loops/tuKA5f3zpzkKKejJx)
+//        System.out.println(RectangleRotation.recRot(6, 4));
 
         //Minimum Distribution Triplet (https://app.codesignal.com/challenge/LPAySw9MQXCi7934W)
         int[] a = {4, 30};
-        int[] b = {6, 12, 20};
-        int[] c = {10, 37};
+        int[] b = {6, 12, 20, 7 ,8, 9, 100};
+        int[] c = {10, 37, 7, 6, 5, 4, 3};
         System.out.println(MinDistTriplet.finder(a ,b, c));
 
     }
--- Java/com.algorithm-practice/src/com/company/MinDistTriplet.java
+++ Java/com.algorithm-practice/src/com/company/MinDistTriplet.java
@@ -5,10 +5,17 @@ public class MinDistTriplet {
     public static int finder(int[] a, int[] b, int[] c) {
 
         int globalSmallest = Integer.MAX_VALUE;
+        StringBuilder sb = new StringBuilder("!");
 
         for (int i = 0; i < a.length; i ++){
+            sb.append("-");
+            System.out.println(sb.toString());
             for (int j = 0; j < b.length; j++){
+                sb.append("_");
+                System.out.println(sb.toString());
                 for (int k = 0; k < c.length; k++){
+                    sb.append("*");
+                    System.out.println(sb.toString());
                     int smallestXandY = Integer.MAX_VALUE;
                     int smallestYandZ = Integer.MAX_VALUE;
                     int smallestZandX = Integer.MAX_VALUE;
