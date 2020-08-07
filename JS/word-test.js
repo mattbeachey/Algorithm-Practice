373dce74d3fed0526c52ef6e02f48c8f6ce974b3
testuser <testuser@example.com>
2020-08-07 11:10:50 -0500

    fixed undefined issue, determined 1st nonoccuring word

--- JS/word-test.js
+++ JS/word-test.js
@@ -16,9 +16,9 @@ function wordReturn(word){
     let letterPicker = Math.floor(Math.random()*27);
     if (letterPicker == 26 && word.length > 0){
         return word;
-    } else if (letterPicker < 26){
-        letterPicker = Math.floor(Math.random()*25);
-        word += String.fromCharCode(letterPicker + 96);
+    } else {
+        letterPicker = Math.floor(Math.random()*26);
+        word += String.fromCharCode(letterPicker + 97);
         return wordReturn(word);
     }
 }
@@ -27,15 +27,20 @@ function wordReturn(word){
 writeWords(1000);
 
 for (let i = 0; i < finalWordArray.length; i++){
-    if (finalWordArray[i] !== undefined){
-        if (isNaN(resultsObject[finalWordArray[i].length])){
-            resultsObject[finalWordArray[i].length] = 1;
-        } else {
-            resultsObject[finalWordArray[i].length] += 1;
-        }
+    if (isNaN(resultsObject[finalWordArray[i].length])){
+        resultsObject[finalWordArray[i].length] = 1;
+    } else {
+        resultsObject[finalWordArray[i].length] += 1;
     }
 }
 
+for (let i = 0; i < Object.keys(resultsObject).length; i++){
+    if (resultsObject[i+1] !== undefined){
+        continue;
+    } else {
+        resultsObject["firstNonOccurence"] = (i + 1) + " letter word";
+        break;
+    }
+}
 
-console.log(resultsObject);
-// console.log("Here comes the space" + String.fromCharCode(32) + "was it there???");
+return resultsObject;
