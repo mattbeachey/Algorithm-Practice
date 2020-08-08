4aa2ff88601579d400a965a22d1805e801358840
Matt Beachey <mattbeachey@gmail.com>
2020-08-08 14:09:29 -0500

    checking for actual duplicate words

--- JS/word-test.js
+++ JS/word-test.js
@@ -3,15 +3,31 @@
 
 const finalWordArray = []
 const resultsObject = {};
+const wordSet = new Set();
+const repeatingWords = {};
+let dupsCounted = 0;
 
 function writeWords(wordCount){
     for(let i = 0; i < wordCount; i++){
         let word = "";
-        finalWordArray.push(wordReturn(word));
+        const newWord = wordReturn(word);
+        finalWordArray.push(newWord);
+        wordSet.add(newWord)
+        //check if new word was a duplicate by comparing the set with the array
+        if (wordSet.size + dupsCounted < finalWordArray.length){
+            dupsCounted++;
+            if (isNaN(repeatingWords[newWord])){
+                repeatingWords[newWord] = 1;
+            } else {
+                repeatingWords[newWord] += 1;
+            }
+            
+        }
     }
     return finalWordArray;
 }
 
+//recursive helper function to generate words randomly
 function wordReturn(word){
     let letterPicker = Math.floor(Math.random()*27);
     if (letterPicker == 26 && word.length > 0){
@@ -24,8 +40,9 @@ function wordReturn(word){
 }
 
 
-writeWords(10000);
+writeWords(100000);
 
+//analysic after generating words
 for (let i = 0; i < finalWordArray.length; i++){
     if (isNaN(resultsObject[finalWordArray[i].length])){
         resultsObject[finalWordArray[i].length] = 1;
@@ -46,6 +63,13 @@ for (let i = 0; i < Object.keys(resultsObject).length; i++){
 
 resultsObject["Most Common Word-Length"] = mostOccur[0];
 
+//longest repeated word
+let longest = "";
+for (let i = 0; i < Object.keys(repeatingWords).length; i++){
+    //finish
+}
+
 console.log(resultsObject);
+// console.log(repeatingWords);
 
 
