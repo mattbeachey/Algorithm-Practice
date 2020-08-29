529bf2d92dcee436663d8f599c97689d6aafea91
zolapineles <mattbeachey@gmail.com>
2020-08-29 13:58:25 -0500

    v close

--- JS/convertToRoman.js
+++ JS/convertToRoman.js
@@ -13,7 +13,7 @@ function convertToRoman(num) {
     return finalString;
   }
   
-console.log(convertToRoman(36));
+console.log(convertToRoman(988));
   
 // let number = 100;
 // console.log(number.toString()[0] !== "5");
@@ -21,19 +21,42 @@ console.log(convertToRoman(36));
 
 function recur(num){
     let nearest = {}
+    let leftPrecede = false;
+    let getPrecede = false;
     for (const [letter, val] of Object.entries(roms)){
-        if (num >= (val - (val/10)) && val.toString()[0] !== "5"){
-          nearest[letter] = val;
-          break;
-        } 
-        if (num >= (val - (val/5)) && val.toString()[0] !== "1"){
+        if (getPrecede === false){
+            if (num >= val){
+                nearest[letter] = val;
+                break;
+            } 
+    
+            if (num >= (val - (val/10)) && val.toString()[0] !== "5"){
+                leftPrecede = true;
+                nearest[letter] = val;
+                getPrecede = true;
+            } 
+            if (num >= (val - (val/5)) && val.toString()[0] !== "1"){
+                leftPrecede = true;
+                nearest[letter] = val;
+                getPrecede = true;
+            } 
+        } else if(val.toString()[0] !== "5"){
             nearest[letter] = val;
             break;
-          } 
+        } else {
+            continue;
+        }
 
-      }
-  
-    totals[Object.keys(nearest)[0]] = Math.floor(num / Object.values(nearest)[0]);
-    let remainder = num % Object.values(nearest)[0];
-    remainder > 0 ? recur(remainder) : console.log("all done");
+    }
+
+    if (leftPrecede === false){
+        totals[Object.keys(nearest)[0]] = Math.floor(num / Object.values(nearest)[0]);
+        let remainder = num % Object.values(nearest)[0];
+        remainder > 0 ? recur(remainder) : console.log("all done");
+    } else {
+        totals[Object.keys(nearest)[1]] = 1;
+        totals[Object.keys(nearest)[0]] = 1;
+        let remainder = num % (Object.values(nearest)[1] - Object.values(nearest)[0]);
+        remainder > 0 ? recur(remainder) : console.log("all done");
+    } 
 }
