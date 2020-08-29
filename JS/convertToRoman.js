9bc754d18b6637adc4fedd9061ba0b2b974db17c
zolapineles <mattbeachey@gmail.com>
2020-08-29 13:26:59 -0500

    fixing left-preceding rule

--- JS/convertToRoman.js
+++ JS/convertToRoman.js
@@ -2,26 +2,35 @@ const roms = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1}
 let totals = {}
 
 function convertToRoman(num) {
- 
     recur(num);
+    let finalString = "";
+    for (const [letter, quantity] of Object.entries(totals)){
+        for (let i = 0; i < quantity; i++){
+            finalString += letter;
+        }
+    }
 
-
-
-    console.log(totals);
-    return num;
+    return finalString;
   }
   
-  convertToRoman(3900);
+console.log(convertToRoman(36));
   
+// let number = 100;
+// console.log(number.toString()[0] !== "5");
+
 
 function recur(num){
     let nearest = {}
     for (const [letter, val] of Object.entries(roms)){
-        if (num >= val){
-            
+        if (num >= (val - (val/10)) && val.toString()[0] !== "5"){
           nearest[letter] = val;
           break;
         } 
+        if (num >= (val - (val/5)) && val.toString()[0] !== "1"){
+            nearest[letter] = val;
+            break;
+          } 
+
       }
   
     totals[Object.keys(nearest)[0]] = Math.floor(num / Object.values(nearest)[0]);
