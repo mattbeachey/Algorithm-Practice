7f1b73730b2cad246b6d1f3d2c57f2c332df156c
zolapineles <mattbeachey@gmail.com>
2020-11-22 12:46:21 -0600

    refactored mean groupings algo in JS

---
+++ JS/meanGroupings.js
@@ -0,0 +1,38 @@
+//group the indexes of internal arrays by their mean, 
+//return all grouped indexes in 2d array ordered by the lowest 
+//present value in a given internal array
+let a = [[4, 4], [2,3], [3,3,3], [2,3], [2, 4]]
+
+function overWorld(a){
+    let meanObj = objectMap(a);
+    let finalArray = []
+    for(const array of Object.entries(meanObj)){
+        finalArray.push(array[1]);
+    }
+
+    finalArray.sort((a, b) => {
+        return a[0] - b[0];
+    })
+
+    return finalArray;
+}
+console.log(overWorld(a))
+
+function objectMap(a){
+	let obj = {};
+	for(i=0; i<a.length; i++){
+		let mean = 0;
+		for(j=0; j<a[i].length; j++){
+			mean += a[i][j];
+        }
+        mean /= a[i].length;
+		if(isNaN(obj[mean])){
+			obj[mean] = [i];
+		}else{
+			obj[mean].push(i);
+		}
+
+	}
+	return obj;
+}
+
