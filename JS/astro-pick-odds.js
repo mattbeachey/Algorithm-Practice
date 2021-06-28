6416ecf32fc8f0014cf92054b78eb411bf170121
Matt Beachey <mattbeachey@gmail.com>
2021-06-28 17:44:03 -0500

    fixed pair checker, added timer

--- JS/astro-pick-odds.js
+++ JS/astro-pick-odds.js
@@ -10,27 +10,55 @@ function pairMaker(){
 }
 
 const totalSets = [];
-for (let i = 0; i < 10; i++){
+console.time("total time")
+let progress = ">"
+for (let i = 0; i < 1000000; i++){
+    if (i % 100000 === 0){
+        console.log(progress);
+        progress += ">";
+    }
     totalSets.push(pairMaker())
 }
+console.timeEnd("total time")
+// totalSets.push(    {
+//     '1': 'a',
+//     '2': 'b',
+//     '3': 'c',
+//     '4': 'd',
+//     '5': 'e',
+//     '6': 'f',
+//     '7': 'g',
+//     '8': 'h',
+//     '9': 'i',
+//     '10': 'j',
+//     '11': 'k',
+//     '12': 'l'
+//   })
 
 const results = {
     totalSets: 0,
     noneRight: 0,
     allRight: 0,
+    meanRight: 0, 
+    highestRight: 0
 }
+
+let runningRight = 0;
 
 totalSets.forEach(pair => {
     const pairArray = Object.values(pair);
     let ammountRight = 0;
 
     for (let i = 0; i < pairArray.length; i++){
-        ammountRight += (pairArray[i].charCodeAt() - 97) === i + 1 ? 1 : 0;
+        ammountRight += (pairArray[i].charCodeAt() - 97) === i ? 1 : 0;
     }
-    console.log("ammount right: ", ammountRight)
+    runningRight += ammountRight;
     results.totalSets++;
     results.noneRight += ammountRight === 0 ? 1 : 0;
     results.allRight += ammountRight === 12 ? 1 : 0;
+    results.highestRight = ammountRight > results.highestRight ? ammountRight : results.highestRight;
 })
+
+results.meanRight = (runningRight/results.totalSets)
 
 console.log(results)
