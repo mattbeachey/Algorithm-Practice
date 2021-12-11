5ec73194dfb0bde880b82ecd44133895d952636a
Matt Beachey <mattbeachey@gmail.com>
2021-12-10 19:20:44 -0600

    flesh out full algorithms

--- JS/rebase.js
+++ JS/rebase.js
@@ -1,8 +1,27 @@
 const rebasePeriodInHours = 8;
 const rebasesPerYear = 8760 / rebasePeriodInHours;
+
+// quick calculations 
 const percentToMultiple = percent => (percent + 100)/100;
 const annualToDailyMultiple = apy => Math.pow(apy, 1/rebasesPerYear);
 
-const multiplePerRebase = annualToDailyMultiple(percentToMultiple(80801.8));
 
-console.log(multiplePerRebase)
+
+evenPriceXDaysOut = (days, buyPrice, apy) => {
+    const annualMultiple = percentToMultiple(apy);
+    const dailyMultiple = annualToDailyMultiple(annualMultiple);
+    const multipleUpOverXDays = Math.pow(dailyMultiple, days);
+    const breakEvenPrice = buyPrice / multipleUpOverXDays;
+    return breakEvenPrice;
+}
+
+xPriceXDaysOut = (days, buyPrice, apy, multipleAtXDays) => {
+    const annualMultiple = percentToMultiple(apy);
+    const dailyMultiple = annualToDailyMultiple(annualMultiple);
+    const multipleUpOverXDays = Math.pow(dailyMultiple, days);
+    const xPrice = (buyPrice * multipleAtXDays) / multipleUpOverXDays;
+    return xPrice;
+}
+
+console.log(evenPriceXDaysOut(30, 5000, 80801));
+console.log(xPriceXDaysOut(30, 5000, 80801, 2));
